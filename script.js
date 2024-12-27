bde4afd742b5b3c9a5e1bae505902688615b701e
Abdulrahman <matheo5670@gmail.com>
2024-12-27 16:38:47 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function ShowSidbar() {
+  const sidbar = document.querySelector('.sidbar');
+  sidbar.style.display = 'flex';
+}
+function Hidesidbar() {
+  const sidbar = document.querySelector('.sidbar');
+  sidbar.style.display = 'none';
+}
